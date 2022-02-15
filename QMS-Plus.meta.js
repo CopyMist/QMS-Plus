85c363b995a5c9ee0e58a2a20572fd02b9c2dcbc
IamR3m <klubberzone@gmail.com>
2022-02-15 23:51:14 +0300

    version 0.2.1 - Update repo links - Add updateURL - Add meta.js

---
+++ QMS-Plus.meta.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         QMS Plus
+// @namespace    4PDA
+// @version      0.2.1
+// @description  Юзерскрипт для добавления/исправления функционала QMS на форуме 4PDA
+// @author       CopyMist, R3m
+// @license      https://creativecommons.org/licenses/by-nc-sa/4.0/deed.ru
+// @icon         https://raw.githubusercontent.com/IamR3m/QMS-Plus/master/icon-128.png
+// @homepage     https://4pda.ru/forum/index.php?showtopic=985927
+// @downloadURL  https://raw.githubusercontent.com/IamR3m/QMS-Plus/master/QMS-Plus.user.js
+// @updateURL    https://raw.githubusercontent.com/IamR3m/QMS-Plus/master/QMS-Plus.meta.js
+// @match        https://4pda.ru/forum/*act=qms*
+// @match        https://4pda.to/forum/*act=qms*
+// @match        http://4pda.ru/forum/*act=qms*
+// @match        http://4pda.to/forum/*act=qms*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/arrive/2.4.1/arrive.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/tippy.js/5.2.1/tippy-bundle.iife.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.15/lodash.min.js
+// @resource     backgroundSvg https://raw.githubusercontent.com/IamR3m/QMS-Plus/master/background.svg
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        unsafeWindow
+// ==/UserScript==
